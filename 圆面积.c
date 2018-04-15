1461d5a7d65b04808799da0acb24d29c0f060e24
bryyy123 <437679875@qq.com>
2018-04-15 19:26:58 +0800

    圆面积

---
+++ 圆面积.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+#define PI 3.1415926
+int main()
+{
+	float r,s;
+    printf("«Î ‰»Î∞Îæ∂\n");
+    scanf("%f",&r);
+        s=PI*r*r;
+    printf("PI=%f r=%f\ns=%f\n",PI,r,s);
+    
+ return 0;
+} 
