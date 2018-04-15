88818f0da58c069483770771ed56af441351a8dd
bryyy123 <437679875@qq.com>
2018-04-15 19:34:06 +0800

    书p112.5

---
+++ 书p112.5.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+#include"math.h"
+int main()
+{
+	int x,n;
+	printf("请输入一个小于1000的正数n：\n");
+	scanf("%d",&n);
+	if(n>=1000)
+	{
+		printf("输入的数不符合要求，请重新输入:\n");
+				
+	}
+	    scanf("%d",&n);
+		x=sqrt(n);
+	    printf("平方根的整数部分是：%d\n",x);	
+		
+	return 0;
+} 
